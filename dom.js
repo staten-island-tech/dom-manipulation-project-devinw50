ce57fcb9218f40b07c0fb3c6236f9c3ea659bcc8
devinw50 <addicteddmg@gmail.com>
2023-10-17 10:13:29 -0400

    wer
    werwer

--- dom.js
+++ dom.js
@@ -1,13 +1,12 @@
 //const form = dpocument.querySelector("#form")
 const DOMSelectors = {
     form: document.querySelector("#form"),
-firstName: docuument.querySelector(".first-name"),
+firstName: document.querySelector(".first-name"),
 h2s:document.querySelectorAll("h2"),
-    //select the textbox
-//select all the h2s in one property
 };
-//console.log(DOMSelectors.h2s);
-DOMSelectors.form.addEventListener("submit", function(event){
+
+
+DOMSelectors.form.addEventListener("#submit", function(event){
     event.preventDefault();
     console.log(DOMSelectors.firstName.value);
 DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value)
