6bd5093cd6cd302c0ba0c2ebf9d48c9e61266d39
devinw50 <addicteddmg@gmail.com>
2023-10-25 10:15:25 -0400

    r
    erg

--- dom.js
+++ dom.js
@@ -1,20 +1,24 @@
-//const form = dpocument.querySelector("#form")
+//const form = dpocument.querySelector("#form")\
+//const album = 
 const DOMSelectors = {
     form: document.querySelector("#form"),
 firstName: document.querySelector(".first-name"),
 h2s:document.querySelectorAll("h2"),
 };
 
-
-DOMSelectors.form.addEventListener("submit", function(event) {
-    event.preventDefault();
+DOMSelectors.form.addEventListener("submit", function(event) {event.preventDefault();
+document
+.querySelector("gallery");
+console.log();
+DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value);
 });
-    //document
-   // .querySelector("gallery")
+
+//document
+ //   .querySelector("gallery")
   //  .insertAdjacentHTML (
-  //      "afterbegin",
+ //       "afterbegin",
 
  //       <div class="card">${DOMSelectors.firstName.value}</div>
  //   )
-/////DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value)
+//DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value)
 //})
