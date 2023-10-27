1c459920aedfb3217abfc013e6eda24861079d69
devinw50 <addicteddmg@gmail.com>
2023-10-27 10:13:25 -0400

    567
    567

--- dom.js
+++ dom.js
@@ -1,30 +1,31 @@
 //const form = dpocument.querySelector("#form")\
 //const pokemon = 
 
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+   h2s: document.querySelectorAll("h2"),
+    h3s: document.querySelectorAll("h3"),
+    button: document.getElementById("btn"),
+    input: document.querySelector("#input"),
+    input2: document.querySelector("#input2"),
+   delete: document.querySelector("#delete"),
+};
 
-DOMSelectors.h2s.forEach((el) => el.textContent = DOMSelectors.firstName.value);
-DOMSelectors.form.addEventListener("submit", function(event) {event.preventDefault()});
+DOMSelectors.form.addEventListener("submit", function(event) 
+{event.preventDefault()});
 const card = DOMSelectors.button.addEventListener("click", function(){
     let input= DOMSelectors.input.value;
     let input2= DOMSelectors.input2.value;
-    DOMSelectors.form.insertAdjacentHTML("beforeend",
-    `<div class="gallery">
-    <div class="card">
-        <h2 class="card-header" id="column">${input}</h2>
-        <h3 class="card-info" id="column">${input2}</h3>  
+    DOMSelectors.form.insertAdjacentHTML("beforeend"
+    `<div class="gallery">,
+    <div class="card">,
+        <h2 class="card-header" id="column">${input}</h2>,
+        <h3 class="card-info" id="column">${input2}</h3>  ,
         </div>,
-    <button type="submit" id="delete">Delete</button>`);
-}
+    <button type="submit" id="delete">Delete</button>`)
+})
 
-    const DOMSelectors = {
-        form: document.querySelector("#form"),
-       h2s: document.querySelectorAll("h2"),
-        h3s: document.querySelectorAll("h3"),
-        button: document.getElementById("btn"),
-        input: document.querySelector(#input),
-        input2: document.querySelector(#input2),
-       delete: document.querySelector(#delete),
-    });
+
     
     function backgroundAndText (background, text){
         background.style.backgroundColor = "red"
@@ -40,3 +41,14 @@ const card = DOMSelectors.button.addEventListener("click", function(){
  //   )
 //DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value)
 //})
+
+//function remove (){
+ //  let btns= document.querySelectorAll("");
+    //console.log()
+  //  btns.forEach((button)=> button.addEventListener("click", function (event){
+  //      console.log(event.target.parentElement);
+ //   })
+ //   );
+//}
+
+//remove();
