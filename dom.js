09278f661845074255460125a2114d8de1f5bea6
devinw50 <addicteddmg@gmail.com>
2023-11-06 10:14:39 -0500

    Update dom.js

--- dom.js
+++ dom.js
@@ -9,11 +9,11 @@ const DOMSelectors = {
     input2: document.querySelector("#input2"),
    delete: document.querySelector("#delete"),
 };
-const card = DOMSelectors.button.addEventListener("click", function(event){
-    event.preventDefault();
+
+const card = DOMSelectors.button.addEventListener("click", function(){
     let input= DOMSelectors.input.value;
     let input2= DOMSelectors.input2.value;
-    DOMSelectors.form.insertAdjacentHTML("beforeend"
+    DOMSelectors.form.insertAdjacentHTML("afterend",
     `<div class="gallery">,
     <div class="card">,
         <h2 class="card-header" id="column">${input}</h2>,
@@ -21,13 +21,13 @@ const card = DOMSelectors.button.addEventListener("click", function(event){
         </div>,
     <button type="submit" id="delete">Delete</button>`)
 })
-
+console.log(card);
 
     
   
-//document
- //   .querySelector("gallery")
-  //  .insertAdjacentHTML (
+// document
+//    .querySelector("gallery")
+//    .insertAdjacentHTML (
  //       "afterbegin",
 
  //       <div class="card">${DOMSelectors.firstName.value}</div>
@@ -35,13 +35,13 @@ const card = DOMSelectors.button.addEventListener("click", function(event){
 //DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value)
 //})
 
-//function remove (){
- //  let btns= document.querySelectorAll("");
-    //console.log()
-  //  btns.forEach((button)=> button.addEventListener("click", function (event){
-  //      console.log(event.target.parentElement);
- //   })
- //   );
-//}
+function remove (){
+   let btns= document.querySelectorAll("");
+    console.log()
+    btns.forEach((button)=> button.addEventListener("click", function (event){
+        console.log(event.target.parentElement);
+    })
+    );
+}
 
-//remove();
+remove();
